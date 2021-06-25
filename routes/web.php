91bd607dd83c451a44db05be17a8d650f8c0c421
RawAndHie <ductath2008041@fpt.edu.vn>
2021-06-25 18:20:42 +0700

    raw

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListController;
+use App\Http\Controllers\UiController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[IndexController::class,'index']);
+Route::get('/list', [ListController::class, 'listTables']);
+Route::get('/form', [FormController::class, 'form']);
+Route::get('/calendar',[CalendarController::class, 'calendar']);
+Route::get('/ui', [UiController::class, 'ui']);
+
